4a57c37840657eae8ecfd541098b9b85ada1fdc2
Shimakenize <Shimakenize@users.noreply.github.com>
2026-05-27 02:04:19 +0000

    deploy: Shimakenize/DailyReminder@2441f451a1251cda47c9f84e21107906f0f80639

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1871538341" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3098511848" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
